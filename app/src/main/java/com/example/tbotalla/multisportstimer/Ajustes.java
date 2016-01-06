ba46ef757f4a414e997a5390714ae6d979701762
Tomas Botalla <tbotalla@localhost.localdomain>
2016-01-06 19:06:01 -0300

    implementacion inicial de pantalla de ajustes usando spinners

--- app/src/main/java/com/example/tbotalla/multisportstimer/Ajustes.java
+++ app/src/main/java/com/example/tbotalla/multisportstimer/Ajustes.java
@@ -2,15 +2,36 @@ package com.example.tbotalla.multisportstimer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by tbotalla on 05/01/16.
  */
 public class Ajustes extends Activity {
+
+    private Spinner cmbRoundNumber;
+    private Spinner cmbRoundAmount;
+    private Spinner cmbRoundTime;
+    private Spinner cmbRestTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ajustes);
+
+        // Creacion del adaptador para el spinner de rounds
+        ArrayAdapter<CharSequence> roundNumberAdapter = ArrayAdapter.createFromResource(this,
+                R.array.numbers_array, android.R.layout.simple_spinner_item);
+
+        cmbRoundNumber = (Spinner)findViewById(R.id.CmbRoundNumber);
+
+        roundNumberAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        cmbRoundNumber.setAdapter(roundNumberAdapter);
+
+
+
     }
 }
